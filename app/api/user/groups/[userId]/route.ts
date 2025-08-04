ce83b69e04996da75080792db4f077fec5eff125
Azeem <mazeem.ajm@gmail.com>
2025-08-04 15:50:25 +0530

    fixed the GET function now in different file

--- app/api/user/groups/[userId]/route.ts
+++ app/api/user/groups/[userId]/route.ts
@@ -2,15 +2,18 @@ import { NextResponse } from "next/server";
 import { connectToDB } from "@/app/lib/mongoose";
 import { Group } from "@/app/models/groups";
 import mongoose from "mongoose";
+interface RouteParams {
+  params: Promise<{ userId: string }>;
+}
 
 export async function GET(
-  req: Request,
-  context: { params: { userId: string } }
+  request: Request,
+  { params }: RouteParams 
 ) {
   try {
     await connectToDB();
 
-    const userId = context.params.userId;
+    const { userId } = await params;
 
     const userObjectId = new mongoose.Types.ObjectId(userId);
 
