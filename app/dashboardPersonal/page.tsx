ebf5a897e5bf577bc166bb326ca265081f5fae35
Azeem <mazeem.ajm@gmail.com>
2025-10-17 14:30:28 +0530

    fixing notif

--- app/dashboardPersonal/page.tsx
+++ app/dashboardPersonal/page.tsx
@@ -88,7 +88,7 @@ export default function DashboardPage() {
     });
 
     // Send the subscription object to your backend to save it
-    const res = await fetch('/api/save-subscription', {
+    const res = await fetch('/api/saveSubscription', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ subscription: subscription, userId: session.user.id }),
